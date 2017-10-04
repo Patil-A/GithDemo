102f3d95cefab1502be47a3080553eaebacaf3d0
webwerks <webwerks@webwerkss-MacBook-Pro-7.local>
2017-10-04 11:43:39 +0530

    My 1st commit to Github

---
+++ GithDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GithDemo
+//
+//  Created by webwerks on 04/10/17.
+//  Copyright © 2017 webwerks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GithDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GithDemo
+//
+//  Created by webwerks on 04/10/17.
+//  Copyright © 2017 webwerks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
